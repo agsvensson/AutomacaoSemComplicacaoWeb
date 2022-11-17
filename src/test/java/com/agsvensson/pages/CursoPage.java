8aed691d1c1f24aa84460f013579587d1ef16c92
Arthur Svensson <guimaraesarthur86@yahoo.com.br>
2022-11-17 19:15:36 -0300

    Até aula 10 - Page Factory

---
+++ src/test/java/com/agsvensson/pages/CursoPage.java
@@ -0,0 +1,22 @@
+package com.agsvensson.pages;
+
+import com.agsvensson.maps.CursoMap;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
+public class CursoPage {
+    WebDriver driver;
+    CursoMap cursoMap;
+
+    public CursoPage(WebDriver driver) {
+        this.driver = driver;
+        cursoMap = new CursoMap();
+        PageFactory.initElements(driver, cursoMap);
+    }
+
+    public String getTitulo2() {
+        return cursoMap.txtTitulo.getText();
+    }
+}
